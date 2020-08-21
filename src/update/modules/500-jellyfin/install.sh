1001758165601f2c591dc80b644465aafc6a61e0
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-21 16:40:22 -0400

    Trying to increase max_user_watches to work around weird out of space errors when building jellyfin?

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo fs.inotify.max_user_watches=524288 | \
+		sudo tee -a /etc/sysctl.d/99-sysctl.conf && sudo sysctl --system
+
 install-nginx-location /tmp/jellyfin/jellyfin.conf
 install-aur https://aur.archlinux.org/jellyfin.git
