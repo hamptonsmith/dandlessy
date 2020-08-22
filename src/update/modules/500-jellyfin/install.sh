c3fb78e9b53ab5a2d14ebcd5da21d801e840ffa9
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-22 10:37:39 -0400

    One more try at Jellyfin autosetup.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -3,10 +3,6 @@
 install-aur https://aur.archlinux.org/jellyfin-server-bin.git
 install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 
-cp /tmp/jellyfin/system.xml /var/lib/jellyfin/config
-chown jellyfin:jellyfin /var/lib/jellyfin/config
-chmod 644 /var/lib/jellyfin/config/system.xml
-
 # Jellyfin won't init if its config directory already exists :eyeroll:, so we start it, let it
 # generate its config file, then stop it and perform surgery in-place.
 systemctl start jellyfin.service
