f6665e5441fb564ff852a3a1e051248e8f938b1b
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-21 21:40:58 -0400

    Enable jellyfin service.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -3,4 +3,6 @@
 install-aur https://aur.archlinux.org/jellyfin-server-bin.git
 install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 
+systemctl enable jellyfin.service
+
 install-nginx-location /tmp/jellyfin/jellyfin.conf
