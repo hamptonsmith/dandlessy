7b3adf7188ccba97b5bf1d2a6780a5aa1e6f3291
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-21 21:36:24 -0400

    Trying a different jellyfin package.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-install-aur https://aur.archlinux.org/dotnet-core-3.0.git
+install-aur https://aur.archlinux.org/jellyfin-server-bin.git
+install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 
 install-nginx-location /tmp/jellyfin/jellyfin.conf
-install-aur https://aur.archlinux.org/jellyfin.git
