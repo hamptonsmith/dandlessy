478a539c4bd0fd67e7880f6acfb10b03b9118eed
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-22 13:23:02 -0400

    Jellyfin dirs for real real.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -6,15 +6,7 @@ install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 mkdir -p /mnt/fossil/config/jellyfin
 chown jellyfin:jellyfin /mnt/fossil/config/jellyfin
 
-mkdir -p /var/cache/jellyfin
-chown jellyfin:jellyfin /var/cache/jellyfin
-
-mkdir -p /var/db/jellyfin
-chown jellyfin:jellyfin /var/db/jellyfin
-
-mkdir -p /var/log/jellyfin
-chown jellyfin:jellyfin /var/log/jellyfin
-
+systemctl enable jellyfin-config-gen.service
 systemctl enable jellyfin.service
 
 install-nginx-location /tmp/jellyfin/jellyfin.conf
