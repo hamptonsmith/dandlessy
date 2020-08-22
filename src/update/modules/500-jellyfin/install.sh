151ec6283e8d5bcfd8378faf8d56416fa4f0220b
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-21 22:15:14 -0400

    Config jellyfin and reverse proxy updates.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -3,6 +3,10 @@
 install-aur https://aur.archlinux.org/jellyfin-server-bin.git
 install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 
+cp /tmp/jellyfin/system.xml /var/lib/jellyfin/config
+chown jellyfin:jellyfin /var/lib/jellyfin/config
+chmod 644 /var/lib/jellyfin/config/system.xml
+
 systemctl enable jellyfin.service
 
 install-nginx-location /tmp/jellyfin/jellyfin.conf
