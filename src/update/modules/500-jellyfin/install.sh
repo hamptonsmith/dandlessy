2751394aa62c298202057006d9f32582757f5aac
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-22 10:59:15 -0400

    Abandoned attempts to auto-config jellyfin, and now a first attempt at simply persisting jellyfin config between updates.

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -3,16 +3,7 @@
 install-aur https://aur.archlinux.org/jellyfin-server-bin.git
 install-aur https://aur.archlinux.org/jellyfin-web-bin.git
 
-# Jellyfin won't init if its config directory already exists :eyeroll:, so we start it, let it
-# generate its config file, then stop it and perform surgery in-place.
-systemctl start jellyfin.service
-sleep 2
-systemctl stop jellyfin.service
-sed -i 's:<BaseUrl />:<BaseUrl>/media</BaseUrl>:g' \
-		/var/lib/jellyfin/config/system.xml
-sed -i 's:<IsStartupWizardCompleted>false:<IsStartupWizardCompleted>true:g' \
-		/var/lib/jellyfin/config/system.xml
-
+mkdir -p /mnt/fossil/config
 systemctl enable jellyfin.service
 
 install-nginx-location /tmp/jellyfin/jellyfin.conf
