4fe924c4c422552ccaad3c292fa0e542852979e7
Hampton Smith <1743291+hamptonsmith@users.noreply.github.com>
2020-08-21 21:25:56 -0400

    Got rid of weird duplicate import

--- src/update/modules/500-jellyfin/install.sh
+++ src/update/modules/500-jellyfin/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 install-aur https://aur.archlinux.org/dotnet-core-3.0.git
-install-aur https://aur.archlinux.org/dotnet-core-3.0.git
 
 install-nginx-location /tmp/jellyfin/jellyfin.conf
 install-aur https://aur.archlinux.org/jellyfin.git
